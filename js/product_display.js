6b6a9b241e8843f731dc53255cddeb92133b989d
Sirinya Phakoom <sphakoom@ucsd.edu>
2016-03-07 20:32:47 -0800

    Adding modal code

--- js/product_display.js
+++ js/product_display.js
@@ -5,142 +5,39 @@ $(document).ready(function() {
 function displayProducts() {
   
   var url = window.location.pathname;
-  var category = "products/"+ url.substr(url.lastIndexOf('/')).replace("html", "json");
-
-  console.log(url);
-  console.log(category);
+  var category = "products"+ url.substr(url.lastIndexOf('/')).replace("html", "json");
 
   $.getJSON(category, function(data) {  
-    console.log(data);
+    var i = 1;
     $.each(data, function(){    
       var name =  this['name'];
       var thumb = this['thumb'];
-
-      console.log(name);
+      var img = this['img'];
+      var desc = this['description'];
+      var price = this['price'];
 
       $("#productGrid").append(
-        "<div class='column'><div id='item'" + 1 
-        + " class='ui card'><div class='image'><img src='"+ thumb + "'>"
+        "<div class='column'><div id='item" + i 
+        + "' class='ui card'><div class='image'><img src='"+ thumb + "'>"
         + "</div><div class='content'><a class='header'>" + name 
         + "</a></div></div></div>");
       
+      $("#productModals").append(
+        "<div class='ui modal item" + i
+        + "'> <i class='close icon'></i>"
+        + "<div class='content'>"
+        + "<img class='ui centered image' src='"+ img +"'>"
+        + "<div class='item-desc'>"
+        + "<p class='item-title'>" + name + "</p>"
+        + "<p class='item-subtitle'>" + description + "</p>"
+        + "<p class='item-price'>" + price + "</p>"
+        + "<button class='ui blue button'>Inquire</button></div></div></div>"
+        );
+
+      i++;
     });
   })
   .error(function(data){
-    console.log('error!');
+    console.log('Error!');
   });
 };
-
-/*
-  var testArray = ["test1","test2","test3","test4"];
-  var vPool = "";
-
-  jQuery.each(testArray, function(i, val) {
-    vPool += val + "<br /> is the best <br />";
-  });
-
-  //We add vPool HTML content to #myDIV
-  $('#myDIV').html(vPool);
-});
-
-
-          <!-- Row 1 -->
-          <div class="column">
-            <div id="item1" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/1-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">ART NOUVEAU STYLE CUFF</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item2" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/2-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">ONYX & MARCASITE RING</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item3" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/3-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">HOBE GOLD-TONE BRACELET</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item4" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/4-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">LAVALIER CHAIN</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item5" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/5-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">SIGNED COPPER NECKLACE</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item6" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/6-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">SAPPHIRE RING</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item7" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/7-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">SHORT NECKLACE</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item8" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/8-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">SILVER EARRINGS</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="column">
-            <div id="item9" class="ui card">
-              <div class="image">
-                <img src="images/estatevintage/9-thumb.png">
-              </div>
-              <div class="content">
-                <a class="header">PAINTED BUTTONS</a>
-              </div>
-            </div>
-          </div>
-          */
